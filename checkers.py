51c2f5f23c26c26ad1e0b53d1cdb781e320d6281
Mark <markchristiansen4224@gmail.com>
2020-11-10 00:14:51 +0000

    Clean up finding adjacent squares

--- checkers.py
+++ checkers.py
@@ -27,18 +27,14 @@ class Piece:
         self._colour = colour
         self._position = position
         self._king = False
-        if colour == Colour.WHITE:
-            self._direction = Direction.UP
-        else:
-            self._direction = Direction.DOWN
+        self._direction = Direction.UP if colour == Colour.WHITE else Direction.DOWN
 
     @property
     def colour(self):
         return self._colour
 
     def move(self, direction: MoveDirection):
-        self._position = [sum(x) for x in zip(self._position, [1, 1] if direction == MoveDirection.UP_RIGHT else
-        [1, -1] if direction == MoveDirection.UP_LEFT else [-1, 1] if direction == MoveDirection.DOWN_RIGHT else [-1, -1])]
+        self._position = Game.adj_square(self._position, direction)
 
     @property
     def other_colour(self):
@@ -77,8 +73,10 @@ class Board:
 
 
 class Game:
-    def __init__(self):
+    def __init__(self, Black_AIClass, White_AIClass):
         self._board = Board()
+        self._black_ai = Black_AIClass(self, Colour.BLACK)
+        self._white_ai = White_AIClass(self, Colour.WHITE)
         self._white = [Piece(Colour.WHITE, [0, 1]), Piece(Colour.WHITE, [0, 3]), Piece(Colour.WHITE, [0, 5]),
                        Piece(Colour.WHITE, [0, 7]), Piece(Colour.WHITE, [1, 0]), Piece(Colour.WHITE, [1, 2]),
                        Piece(Colour.WHITE, [1, 4]), Piece(Colour.WHITE, [1, 6]), Piece(Colour.WHITE, [2, 1]),
@@ -113,11 +111,8 @@ class Game:
             # can't jump off the board
             if 0 > jump_square[0] or jump_square[0] > 7 or 0 > jump_square[1] or jump_square[1] > 7:
                 return False, None
-            # Can't jump onto square with own piece
-            if self._board.state[new_square[0]][new_square[1]] == piece.colour:
-                return False, None
-            # jump blocked
-            if self._board.state[new_square[0]][new_square[1]] == piece.other_colour:
+            # Can't jump onto square with any piece
+            if self._board.state[new_square[0]][new_square[1]] != Colour.BLANK:
                 return False, None
             return True, MoveType
         return True
@@ -131,43 +126,41 @@ class Game:
                 return True
             if m_type == MoveType.JUMP:
                 # OK. I can do a jump, how to give the option of multiple jumps?  Maybe leave and figure out later
-                # TODO: how to check which piece was taken and remove them?
+                new_square = self.adj_square(piece.position, direction)
                 self._board.state[piece.position[0]][piece.position[1]] = Colour.BLANK
+                self._board.state[new_square[0]][new_square[1]] = Colour.BLANK
+                for piece in self._black:
+                    if piece.position == new_square:
+                        piece.remove_piece()
                 piece.move(direction)
                 piece.move(direction)
+                # TODO: multi jump
                 if self.check_piece_can_take(piece):
                     pass
                 return True
         return False
 
     def check_piece_can_take(self, piece: Piece):
+        # Check 4 directions
         for element in [MoveDirection.DOWN_RIGHT, MoveDirection.DOWN_LEFT, MoveDirection.UP_LEFT, MoveDirection.UP_RIGHT]:
             if (element == MoveDirection.DOWN_LEFT or element == MoveDirection.DOWN_RIGHT) and piece.direction == Direction.UP and not piece.king:
                 # Can't take in the wrong direction
                 continue
-            new_square = [sum(x) for x in zip(piece.position, [1, 1] if element == MoveDirection.UP_RIGHT else
-            [1, -1] if element == MoveDirection.UP_LEFT else [-1, 1] if element == MoveDirection.DOWN_RIGHT else [
-                -1, -1])]
-            if self._board.state[new_square[0]][new_square[1]]
-    # TODO:  Need to be able to check the direction that each piece is meant to be travelling in.  Guess white means up
-    def check_take_avail(self, side):
-        for i in range(8):
-            for j in range(8):
-                if self._board.state[i][j] == Colour.WHITE and side == Colour.WHITE:
-                    if i < 6 and j < 6:
-                        if self._board.state[i+1][j+1] == Colour.BLACK and self._board.state[i + 2][j + 2] == Colour.BLANK:
-                            return True
-                    if i < 6 and j > 1:
-                        if self._board.state[i+1][j-1] == Colour.BLACK and self._board.state[i + 2][j - 2] == Colour.BLANK:
-                            return True
-                if self._board.state[i][j] == Colour.BLACK and side == Colour.BLACK:
-                    if i > 1 and j < 6:
-                        if self._board.state[i+1][j+1] == Colour.BLACK and self._board.state[i + 2][j + 2] == Colour.BLANK:
-                            return True
-                    if i > 1 and j > 1:
-                        if self._board.state[i+1][j-1] == Colour.BLACK and self._board.state[i + 2][j - 2] == Colour.BLANK:
-                            return True
+            new_square = self.adj_square(piece.position, element)
+            jump_square = self.adj_square(new_square, element)
+            # Check these squares are on the board
+            if min(jump_square) < 0 or max(jump_square) > 8:
+                continue
+            if (self._board.state[new_square[0]][new_square[1]] == piece.other_colour and
+               self._board.state[jump_square[0]][jump_square[1]] == Colour.BLANK):
+                return True
         return False
+
+    @staticmethod
+    def adj_square(place, direc):
+        return [sum(x) for x in zip(place, [1, 1] if direc == MoveDirection.UP_RIGHT else
+                [1, -1] if direc == MoveDirection.UP_LEFT else
+                [-1, 1] if direc == MoveDirection.DOWN_RIGHT else [-1, -1])]
 
     def check_blocked(self):
         for i in range(8):
